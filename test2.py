677158ed7502acda04ea0ccd9da7d56d1073fe4f
fast40 <eclfast@gmail.com>
2025-09-15 13:30:07 -0700

    Some messing aroudn

--- test2.py
+++ test2.py
@@ -1,8 +1,23 @@
 from matplotlib import pyplot as plt
+from matplotlib.markers import TICKLEFT
 import numpy as np
 
-x = np.arange(10)
-y = x ** 2
+x = np.arange(30)
+y1 = x
+y2 = x ** 2
+y3 = x ** 3
 
-plt.plot(x, y)
+line, = plt.scatter(x, x**2, label='linear')
+# line.axes.grid()
+plt.grid()
+# line.set_ydata(y2)
+# line.axes.relim()
+# plt.autoscale()
+
+# for thing in dir(line.axes):
+#     print(thing)
+
+# plt.plot(x, y2, label='quadratic')
+# plt.plot(x+3, y1, label='linear2')
+plt.legend()
 plt.show()
